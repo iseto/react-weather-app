78d00a81448cc62a05ba96f9f134ec764afe5f1f
Ignatius Seto <ignatius.seto@accenture.com>
2022-07-14 19:55:35 +0200

    add current weather component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
 import "./App.css";
+import { useState } from "react";
+import { WEATHER_API_KEY, WEATHER_API_URL } from "./components/search/api";
+import CurrentWeather from "./components/current_weather/current-weather";
 import Search from "./components/search/search";
 
 function App() {
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [forecast, setForecast] = useState(null);
+
   const handleOnSearchChange = (searchData) => {
+    const [lat, lon] = searchData.value.split(" ");
+    const currentWeatherFetch = fetch(
+      `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+    );
+
+    const weatherForecastFetch = fetch(
+      `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+    );
+
+    Promise.all([currentWeatherFetch, weatherForecastFetch])
+      .then(async (response) => {
+        const weatherResponse = await response[0].json();
+        const forecastResponse = await response[1].json();
+
+        setCurrentWeather({ city: searchData.label, ...weatherResponse });
+        setForecast({ city: searchData.label, ...forecastResponse });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
     console.log("searchData: ", searchData);
   };
 
+  console.log("currentWeather: ", currentWeather);
+  console.log("forecast: ", forecast);
   return (
     <div className="container">
       <Search onSearchChange={handleOnSearchChange} />
+      {currentWeather && <CurrentWeather data={currentWeather} />}
     </div>
   );
 }
--- src/components/search/api.js
+++ src/components/search/api.js
@@ -1,4 +1,6 @@
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "e4018aca7bd170278c86013641b39bea";
 
 export const geoApiOptions = {
   method: "GET",
