438b7cecdf43fc6c3309f77706fd182640113009
Ignatius Seto <ignatius.seto@accenture.com>
2022-07-14 17:30:29 +0200

    add search component

---
+++ src/components/search/api.js
@@ -0,0 +1,9 @@
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "611984f25dmshf2c6456112ea2f7p1451cbjsn603824005853",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
